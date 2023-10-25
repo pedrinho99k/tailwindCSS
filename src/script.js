791be92beaef597a6440cfd4fec4f47bfa118b2f
pedrinho99k <pedrofsh10@gmail.com>
2023-10-25 16:37:41 -0300

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const backgroundContainer = document.getElementById("background-container");
+const backgroundSelector = document.getElementById("background-selector");
+
+console.log(backgroundSelector.value);
+
+backgroundSelector.addEventListener("change", function() {
+    const selectedValue = backgroundSelector.value;
+
+    if (selectedValue === "1") {
+        backgroundContainer.style.backgroundImage = "url('./images/japan.jpg')";
+    } else if (selectedValue === "2") {
+        backgroundContainer.style.backgroundImage = "url('./images/sweden.jpg')";
+    } else if (selectedValue === "3") {
+        backgroundContainer.style.backgroundImage = "url('./images/new-york.jpg')";
+    }
+});
